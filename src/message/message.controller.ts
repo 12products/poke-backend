cff3a9477ce6a13fb55965dbe0d278782d875458
Alice Zhao <azhao991@gmail.com>
2022-01-12 21:25:49 -0800

    feat: Hook up twilio to send message

--- src/message/message.controller.ts
+++ src/message/message.controller.ts
@@ -1,7 +1,12 @@
-import { Controller } from '@nestjs/common'
+import { Controller, Post } from '@nestjs/common'
 import { MessageService } from './message.service'
 
 @Controller('message')
 export class MessageController {
   constructor(private readonly messageService: MessageService) {}
+
+  @Post()
+  sendMessage(reminderId: string) {
+    return this.messageService.sendMessage(reminderId)
+  }
 }
--- src/message/message.service.ts
+++ src/message/message.service.ts
@@ -1,5 +1,7 @@
 import { Injectable, Logger } from '@nestjs/common'
 import { Cron, CronExpression } from '@nestjs/schedule'
+import { ConfigService } from '@nestjs/config'
+import * as twilio from 'twilio'
 
 import { Message } from '@prisma/client'
 import { DatabaseService } from '../database/database.service'
@@ -7,8 +9,18 @@ import { DatabaseService } from '../database/database.service'
 @Injectable()
 export class MessageService {
   private readonly logger = new Logger(MessageService.name)
+  private twilioClient: twilio.Twilio
+  private twilioPhone: string
 
-  constructor(private readonly db: DatabaseService) {}
+  constructor(
+    private readonly db: DatabaseService,
+    private configService: ConfigService
+  ) {
+    const accountID = this.configService.get<string>('TWILIO_ACCOUNT_ID')
+    const authToken = this.configService.get<string>('TWILIO_AUTH_TOKEN')
+    this.twilioPhone = this.configService.get<string>('TWILIO_PHONE')
+    this.twilioClient = twilio(accountID, authToken)
+  }
 
   async create(reminderId: string): Promise<Message> {
     const message = await this.db.message.create({
@@ -19,9 +31,22 @@ export class MessageService {
       },
     })
 
-    // TODO: Send text via Twilio
+    await this.sendMessage(reminderId)
 
     return message
+  }
+
+  async sendMessage(reminderId: string) {
+    const reminder = await this.db.reminder.findUnique({
+      where: { id: reminderId },
+    })
+
+    const response = await this.twilioClient.messages.create({
+      body: reminder.text,
+      from: this.twilioPhone,
+      to: '+17786978925',
+    })
+    console.log({ response })
   }
 
   @Cron(CronExpression.EVERY_5_MINUTES)
