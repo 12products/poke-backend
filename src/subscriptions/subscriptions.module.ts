8611bdc0da28ad491a426e2265c0fcc7e65b00d8
Anthony Morris <amorriscode@gmail.com>
2022-01-31 20:30:34 -0800

    feat: add subscriptions to a user

---
+++ src/subscriptions/subscriptions.module.ts
@@ -0,0 +1,12 @@
+import { Module } from '@nestjs/common'
+import { SubscriptionsService } from './subscriptions.service'
+import { SubscriptionsController } from './subscriptions.controller'
+
+import { UsersModule } from 'src/users/users.module'
+
+@Module({
+  imports: [UsersModule],
+  controllers: [SubscriptionsController],
+  providers: [SubscriptionsService],
+})
+export class SubscriptionsModule {}
--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -8,5 +8,6 @@ import { DatabaseModule } from 'src/database/database.module'
   imports: [DatabaseModule],
   controllers: [UsersController],
   providers: [UsersService],
+  exports: [UsersService],
 })
 export class UsersModule {}
